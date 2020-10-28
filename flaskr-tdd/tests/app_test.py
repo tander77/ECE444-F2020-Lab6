54a74c90a2f6eedfcd4dcd4293b0108559b0df12
tander77 <32040125+tander77@users.noreply.github.com>
2020-10-28 10:49:05 -0400

    Added a stylesheet and delete functionality
    Also fixed a bug in the login page and added test functions for everything.

--- flaskr-tdd/tests/app_test.py
+++ flaskr-tdd/tests/app_test.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import json
 from pathlib import Path
 
 from project.app import app, init_db
@@ -65,3 +66,8 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+def test_delete_message(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get('/delete/1')
+    data = json.loads(rv.data)
+    assert data["status"] == 1
